98d8a0fb89c5048c39971e5c89413ef31fa5ebd7
Leslie Cazarez <cazarezgilleslie@gmail.com>
2021-04-22 23:51:13 -0700

    Adding my first JS challenge

---
+++ Variables/script.js
@@ -0,0 +1,39 @@
+//Data types in JS
+
+//STRING
+var fruit = "apple";
+var myBreakfast = "Today I'm eating pancakes";
+
+//NUMBERS
+var age = 26;
+var daysUntilExpiration = 7;
+var withDecimals = 10.3;
+
+//BOOLEAN TRUE-FALSE
+var isEdible = true;
+var isRed = false;
+
+//ARRAYS
+var colors = ["blue", "red", "yellow"];
+
+//OBJECTS
+var person = {
+  firstName: "Leslie",
+  lasName: "Cazarez",
+  age: 26,
+  eyeColor: "Brown",
+};
+
+//UNDEFINED Value can be something that has to be calculated
+//Any variable can be emptied, by setting the value to undefined. The type will also be undefined.
+var name;
+var lastname = undefined;
+
+//NULL In JavaScript null is "nothing". It is supposed to be something that doesn't exist.
+var person = {
+  firstName: "Leslie",
+  lasName: "Cazarez",
+  age: 26,
+  eyeColor: "Brown",
+};
+person = null;
